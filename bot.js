f009e3bb9f930a2bd153644532be24e0c8d54a38
Tim-THW <77691248+Tim-THW@users.noreply.github.com>
2021-01-19 19:15:15 +0100

    Musik
    Ersterbot

---
+++ bot.js
@@ -0,0 +1,8 @@
+const {Client} = require('discord.js');
+
+const bot = new Client();
+
+bot.login('ODAxMDA5Mzk0ODU0Nzg5MTI5.YAacIQ.MUg2TKbFiKoe7v6yNlGaQw7dKjo');
+
+bot.on('ready', () => console.log("On!"));
+
