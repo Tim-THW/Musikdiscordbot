49c4a65abcfef576b5496b6b6a2f5ccfa1614ee8
Tim-THW <77691248+Tim-THW@users.noreply.github.com>
2021-01-20 18:49:28 +0100

    V3.0

--- bot.js
+++ bot.js
@@ -1,8 +1,46 @@
-const {Client} = require('discord.js');
-
+const {Client,} = require('discord.js');
+const { play, stop } = require('./commands');
 const bot = new Client();
 
 bot.login('ODAxMDA5Mzk0ODU0Nzg5MTI5.YAacIQ.MUg2TKbFiKoe7v6yNlGaQw7dKjo');
 
 bot.on('ready', () => console.log("On!"));
 
+bot.on('message', (msg)=>{
+      if(msg.author.bot) return;
+
+    const prefix = '?';
+    if (!msg.content.startsWith(prefix)) return;
+
+    const commandName = getCommandName(prefix, msg.content);
+    const args = getCommandArgs(prefix, msg.content);
+
+    if (commandName === 'play')
+    return play(msg, args);
+    else if (commandName ==='stop')
+    return stop(msg, args);
+
+    if (commandName === 'ferlassen')
+    const vc = msg.member.voice.channel;
+    await vc.leave();
+
+    if (commandName === 'Join')
+    const vc = msg.member.voice.channel;
+    await vc.join();
+
+
+
+});
+
+function getCommandName(prefix, content) {
+    return content
+    .slice(prefix.length)
+    .split(' ')[0];
+}
+
+function getCommandArgs(prefix, content) {
+    return content
+    .slice(prefix.length)
+    .split(' ')
+    .slice(1);
+}
---
+++ command.js
@@ -0,0 +1,34 @@
+const downloadYT =require('ytdl-core');
+const searchYT =require('yt-search');
+
+async function play(msg, ...args) {
+    const vc =msg.member.voice.channel;
+
+    const connection = await vc.join();
+    const video = await findVideo(args.join(' '));
+
+    if(video) {
+        const stream = downloadYT(video.url, {filter:  'audioonly' });
+        connection.play(stream,{seek: 0, voliume: 1 });
+
+        await msg.reply(`Jetzt wird gepielt: \'${vido.title}\'.`);
+    }else
+    await msg.reply(`Nichts gefunden`);
+}
+async function findVideo(query) {
+    const result =await searchYT(query);
+    return (result.videos.length > 1)
+      ? result.videos[0]
+      : null;
+}
+
+async function stop(msg) {
+    const vc = msg.member.voice.channel;
+    await vc.lerave();
+
+    await msg.reply('gestoppt');
+}
+
+
+module.exports.play = play;
+module.exports.stop = stop;
